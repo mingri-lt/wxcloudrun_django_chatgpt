5a3427d7b0cb354c56963c47526b2d03000ef97b
mingri-lt <841553693@qq.com>
2023-03-05 00:29:40 +0800

    update

--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -23,4 +23,7 @@ urlpatterns = (
 
     # 获取主页
     url(r'(/)?$', views.index),
+
+    # 对话接口 POST
+    url(r'^^api/dialog(/)?$', views.dialogue),
 )
--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -89,3 +89,14 @@ def update_count(request):
     else:
         return JsonResponse({'code': -1, 'errorMsg': 'action参数错误'},
                     json_dumps_params={'ensure_ascii': False})
+
+def dialogue(request, _):
+    # GET 请求，返回hello world
+    if request.method == 'GET' or request.method == 'get':
+        return JsonResponse({'code': 0, 'data': 'hello world'},
+                            json_dumps_params={'ensure_ascii': False})
+    # POST 请求，返回请求体
+    elif request.method == 'POST' or request.method == 'post':
+        logger.info('dialogue req: {}'.format(request.body))
+        return JsonResponse({'code': 0, 'data': request.body.decode('utf-8')},
+                            json_dumps_params={'ensure_ascii': False})
